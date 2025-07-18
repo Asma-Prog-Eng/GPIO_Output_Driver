561b4506f60f29a32c39a5f47e330b7ddaf99918
Asma Askri <asmaenim@gmail.com>
2025-07-18 16:40:04 +0200

    Update include directories path to include CMSIS required header files

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,18 +1,18 @@
 Src/main.o: ../Src/main.c ../Inc/GPIOA_Output.h \
- C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Device/Include/stm32f4xx.h \
- C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Device/Include/stm32f411xe.h \
- C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/core_cm4.h \
- C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/cmsis_version.h \
- C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/cmsis_compiler.h \
- C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/cmsis_gcc.h \
- C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/mpu_armv7.h \
- C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Device/Include/system_stm32f4xx.h
+ C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/stm32f4xx.h \
+ C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/stm32f411xe.h \
+ C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/core_cm4.h \
+ C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/cmsis_version.h \
+ C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/cmsis_compiler.h \
+ C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/cmsis_gcc.h \
+ C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/mpu_armv7.h \
+ C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/system_stm32f4xx.h
 ../Inc/GPIOA_Output.h:
-C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Device/Include/stm32f4xx.h:
-C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Device/Include/stm32f411xe.h:
-C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/core_cm4.h:
-C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/cmsis_version.h:
-C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/cmsis_compiler.h:
-C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/cmsis_gcc.h:
-C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Include/mpu_armv7.h:
-C:/Users/asmae/OneDrive/Bureau/git/CMSIS/Device/Include/system_stm32f4xx.h:
+C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/stm32f4xx.h:
+C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/stm32f411xe.h:
+C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/core_cm4.h:
+C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/cmsis_version.h:
+C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/cmsis_compiler.h:
+C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/cmsis_gcc.h:
+C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/mpu_armv7.h:
+C:/Users/asmae/OneDrive/Bureau/GPIO_Output_Driver/CMSIS/system_stm32f4xx.h:
