11c6b87223b4ddd93265775b76ace611065d8c74
Asma-Prog-Eng <asmaenim@gmail.com>
2025-07-23 09:27:11 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -1,13 +1,16 @@
 /**
  ******************************************************************************
-Toggle Led connected to PA5 while configuring clock frequency to 50 Mhz
-using PLL as input source clock
+ * @file           : main.c
+ * @author         : Asma Askri
+ * @brief          : Main program body
  ******************************************************************************
- */
+ * Bare metal implementation of a GPIO Output Driver 
+ * Toggle LED connected to PA5 at a PLL clock frequency of 50 Mz
+ * 
+ ******************************************************************************/
 
 #include "GPIOA_Output.h"
 #include "stm32f4xx.h"
-
 
 int main (void) {
 
